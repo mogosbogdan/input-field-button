03b52fdadf69d2261e3b5addd5fdf4e73ff7669a
mogosbogdan <38261124+mogosbogdan@users.noreply.github.com>
2022-12-08 22:44:28 +0200

    name

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     <div className="App">
       <form onSubmit={handleSubmit}>
         <label>
-          Enter a value:
+          Enter a name:
           <input type="text" value={value} onChange={handleChange} />
         </label>
       </form>
