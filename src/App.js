0c7a30f611417b2d7a0a3145b4dbdcc3d70c9b61
mogosbogdan <38261124+mogosbogdan@users.noreply.github.com>
2022-12-08 22:52:19 +0200

    right stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import "./App.css";
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
- 
+  // use the useState hook to manage the value of the input field
+  const [value, setValue] = useState('');
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // set the value of the input field to the value entered by the user
+    setValue(value);
+  }
+
+  const [active, setActive] = useState(false);
+  const handleClick = () => {
+    if (value === "Gabriel" ) {
+      setActive(!active);
+    } else {
+      setActive(active)
+    }
+  };
   return (
 
 
     <div className="App">
-     <div>Hello World</div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter a name:
+          <input type="text" value={value} onChange={handleChange} />
+        </label>
+      </form>
+
+      <div onClick={handleClick}>
+        {!active ? (
+          <button type="button" className="btn btn-primary">
+            {value} e ratat
+          </button>
+        ) : (
+          <button type="button" className="btn btn-danger">
+            True
+          </button>
+        )}
+      </div>
     </div>
   );
 }
